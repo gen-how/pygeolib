1db2652b66bfba2d119a3d640f578d8007261d1f
Jian-Hao Chen <billchen85@gmail.com>
2023-05-07 14:23:50 +0800

    feat: Add some magic functions

--- src/pygeolib/point.py
+++ src/pygeolib/point.py
@@ -11,6 +11,21 @@ class Point2D:
     x: float
     y: float
 
+    def __eq__(self, other: Point2D) -> bool:
+        return (self.x == other.x) and (self.y == other.y)
+
+    def __add__(self, other: Point2D) -> Point2D:
+        return Point2D(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Point2D) -> Point2D:
+        return Point2D(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, factor: float) -> Point2D:
+        return Point2D(self.x * factor, self.y * factor)
+
+    def __truediv__(self, divisor: float) -> Point2D:
+        return Point2D(self.x / divisor, self.y / divisor)
+
     def distance_to(self, other: Point2D) -> float:
         return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
 

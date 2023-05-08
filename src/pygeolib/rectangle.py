70aa8e4c0d0405a95326419ab462e8ce5100776c
Jian-Hao Chen <billchen85@gmail.com>
2023-05-08 14:36:23 +0800

    feat: Implement Rectangle.intersection

--- src/pygeolib/rectangle.py
+++ src/pygeolib/rectangle.py
@@ -2,6 +2,8 @@
 """Implement rectangle class."""
 from __future__ import annotations
 
+from typing import Optional
+
 from .point import Point2D
 
 
@@ -9,10 +11,10 @@ class Rectangle(object):
 
     def __init__(self, pt1: tuple[float, float], pt2: tuple[float, float]):
         self.vertices = [
-            Point2D(pt1[0], pt1[1]),  # Top-left corner
-            Point2D(pt2[0], pt1[1]),  # Top-right corner
-            Point2D(pt2[0], pt2[1]),  # Bottom-right corner
-            Point2D(pt1[0], pt2[1]),  # Bottom-left corner
+            Point2D(pt1[0], pt1[1]),  # Top left corner
+            Point2D(pt2[0], pt1[1]),  # Top right corner
+            Point2D(pt2[0], pt2[1]),  # Bottom right corner
+            Point2D(pt1[0], pt2[1]),  # Bottom left corner
         ]
 
     @property
@@ -30,3 +32,21 @@ class Rectangle(object):
     @property
     def centroid(self) -> Point2D:
         return (self.vertices[0] + self.vertices[3]) / 2
+
+    @property
+    def top_left(self) -> Point2D:
+        return self.vertices[0]
+
+    @property
+    def bottom_right(self) -> Point2D:
+        return self.vertices[2]
+
+    def intersection(self, other: Rectangle) -> Optional[Rectangle]:
+        x1 = max(self.top_left.x, other.top_left.x)
+        y1 = max(self.top_left.y, other.top_left.y)
+        x2 = min(self.bottom_right.x, other.bottom_right.y)
+        y2 = min(self.bottom_right.y, other.bottom_right.y)
+
+        if (x1 < x2) and (y1 < y2):
+            return Rectangle((x1, y1), (x2, y2))
+        return None
---
+++ tests/test_rectangle.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+import unittest
+
+from src.pygeolib import Rectangle
+
+
+class RectangleTestCase(unittest.TestCase):
+
+    def test_intersection_valid(self):
+        a = Rectangle((0, 0), (10, 10))
+        b = Rectangle((5, 5), (15, 15))
+        result = a.intersection(b)
+        assert result != None
+        self.assertEqual(result.top_left.x, 5)
+        self.assertEqual(result.top_left.y, 5)
+        self.assertEqual(result.bottom_right.x, 10)
+        self.assertEqual(result.bottom_right.y, 10)
+
+    def test_intersection_invalid(self):
+        c = Rectangle((0, 0), (5, 5))
+        d = Rectangle((10, 10), (15, 15))
+        result = c.intersection(d)
+        self.assertIsNone(result)

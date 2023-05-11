f1daba982aea7e4923bec7d2bbb0add1a68f0bcb
Jian-Hao Chen <billchen85@gmail.com>
2023-05-11 12:27:16 +0800

    test: Test initialize methods of `Rectangle`

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -6,6 +6,20 @@ from src.pygeolib import Rectangle
 
 class RectangleTestCase(unittest.TestCase):
 
+    def test_init(self):
+        a = Rectangle((0, 0), (10, 10))
+        self.assertEqual(a.top_left.x, 0)
+        self.assertEqual(a.top_left.y, 0)
+        self.assertEqual(a.bottom_right.x, 10)
+        self.assertEqual(a.bottom_right.y, 10)
+
+    def test_init_from_centroid(self):
+        a = Rectangle.from_centroid((0, 0), 10, 10)
+        self.assertEqual(a.top_left.x, -5)
+        self.assertEqual(a.top_left.y, -5)
+        self.assertEqual(a.bottom_right.x, 5)
+        self.assertEqual(a.bottom_right.y, 5)
+
     def test_intersection_valid(self):
         a = Rectangle((0, 0), (10, 10))
         b = Rectangle((5, 5), (15, 15))
@@ -21,7 +35,7 @@ class RectangleTestCase(unittest.TestCase):
         d = Rectangle((10, 10), (15, 15))
         result = c.intersection(d)
         self.assertIsNone(result)
-    
+
     def test_intersection_over_union_valid(self):
         a = Rectangle((0, 0), (10, 10))
         b = Rectangle((5, 5), (15, 15))
@@ -31,4 +45,3 @@ class RectangleTestCase(unittest.TestCase):
         c = Rectangle((0, 0), (5, 5))
         d = Rectangle((10, 10), (15, 15))
         self.assertEqual(0, c.intersection_over_union(d))
-        
